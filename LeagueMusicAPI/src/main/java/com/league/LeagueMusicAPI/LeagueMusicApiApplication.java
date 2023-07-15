98f0ce91e653c90e7a58b87d9fb3f7e213cd386b
Mike1000e <56172395+Mike1000e@users.noreply.github.com>
2023-07-14 18:28:34 -0700

    milestone2 done

---
+++ LeagueMusicAPI/src/main/java/com/league/LeagueMusicAPI/LeagueMusicApiApplication.java
@@ -0,0 +1,13 @@
+package com.league.LeagueMusicAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueMusicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueMusicApiApplication.class, args);
+	}
+
+}
